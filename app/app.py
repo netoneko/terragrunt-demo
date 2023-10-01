26a37243caddd1d49fc38faac1218ba71b630bc3
Kirill Maksimov <netoneko@gmail.com>
2023-10-01 18:50:46 +0300

    add new endpoint to app

--- app/app.py
+++ app/app.py
@@ -1,6 +1,26 @@
 from flask import Flask, request
 from geoip import open_database
 
+def get_client_ip_from_request(request):
+    return request.headers.get("X-Forwarded-For")
+
+
+def get_country_from_client_ip(geoip, client_ip):
+    try:
+        match = geoip.lookup(client_ip)
+        if match:
+            return match.country
+    except Exception as e:
+        app.logger.error(f"Could not resolve client ip {client_ip}, X-Forwarded-For request header might have incorrect value")
+
+
+def get_input_from_request(request):
+    try:
+        return request.get_json()['input']
+    except Exception as e:
+        app.logger.error('Could not get user input, correct format is a POST request {"input":"value"} with application/json content type')
+
+
 def create_app():
     app = Flask(__name__)
     geoip = open_database("./GeoLite2-Country.mmdb")
@@ -17,11 +37,27 @@ def create_app():
             if match:
                 return match.country
         except Exception as e:
-            app.logger.error("Could not resolve client ip %s, X-Forwarded-For request header might have incorrect value")
+            app.logger.error(f"Could not resolve client ip {client_ip}, X-Forwarded-For request header might have incorrect value")
 
         return "Not Available", 500
 
 
+    @app.route("/echo", methods=["POST"])
+    def echo():
+        client_ip = get_client_ip_from_request(request)
+        result = {
+            'source': client_ip,
+            'input': get_input_from_request(request),
+            'country': get_country_from_client_ip(geoip, client_ip),
+        }
+
+        if not result.get('input'):
+            result["error"] = 'Malformed or missing input'
+            return result, 500
+
+        return result
+
     return app
+
 
 app = create_app()
--- app/app_test.py
+++ app/app_test.py
@@ -35,6 +35,21 @@ def test_request_ip_without_header(client):
     assert 500 == response.status_code
 
 def test_request_ip_with_header(client):
-    response = client.get("/ip", headers={'X-Forwarded-For': '17.0.0.1'})
+    response = client.get("/ip", headers={"X-Forwarded-For": "17.0.0.1"})
     assert b"US" == response.data
+    assert 200 == response.status_code
+
+def test_echo_with_correct_parameters(client):
+    response = client.post("/echo", headers={"X-Forwarded-For": "17.0.0.1"}, json={"input": "Hello, world!"})
+    assert {"country": "US", "input": "Hello, world!", "source": "17.0.0.1"} == response.json
+    assert 200 == response.status_code
+
+def test_echo_without_input(client):
+    response = client.post("/echo", headers={"X-Forwarded-For": "17.0.0.1"})
+    assert {"country": "US", "input": None, "source": "17.0.0.1", "error": "Malformed or missing input"} == response.json
+    assert 500 == response.status_code
+
+def test_echo_with_header(client):
+    response = client.post("/echo", json={"input": "Hello, world!"})
+    assert {"country": None, "input": "Hello, world!", "source": None} == response.json
     assert 200 == response.status_code
