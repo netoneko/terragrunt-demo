7a5a1b672f6c375ab0d3ab9237ff928d0d33234a
Kirill Maksimov <netoneko@gmail.com>
2023-10-01 18:26:37 +0300

    app improvements

--- app/app.py
+++ app/app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, request
 from geoip import open_database
 
 def create_app():
     app = Flask(__name__)
-    geoip = open_database('./GeoLite2-Country.mmdb')
+    geoip = open_database("./GeoLite2-Country.mmdb")
 
     @app.route("/")
     def hello():
@@ -11,11 +11,15 @@ def create_app():
 
     @app.route("/ip")
     def ip():
-        match = geoip.lookup('17.0.0.1')
-        if match:
-            return match.country
+        try:
+            client_ip = request.headers.get("X-Forwarded-For")
+            match = geoip.lookup(client_ip)
+            if match:
+                return match.country
+        except Exception as e:
+            app.logger.error("Could not resolve client ip %s, X-Forwarded-For request header might have incorrect value")
 
-        return "Not Available"
+        return "Not Available", 500
 
 
     return app
--- app/app_test.py
+++ app/app_test.py
@@ -27,7 +27,14 @@ def runner(app):
 def test_request_example(client):
     response = client.get("/")
     assert b"Hello, World!" in response.data
+    assert 200 == response.status_code
 
-def test_request_ip(client):
+def test_request_ip_without_header(client):
     response = client.get("/ip")
+    assert b"Not Available" == response.data
+    assert 500 == response.status_code
+
+def test_request_ip_with_header(client):
+    response = client.get("/ip", headers={'X-Forwarded-For': '17.0.0.1'})
     assert b"US" == response.data
+    assert 200 == response.status_code
